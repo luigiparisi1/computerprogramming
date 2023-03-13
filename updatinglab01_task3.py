956d04c48b5de2f2f54e94388ef021f61b8ef093
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-03-13 16:56:11 +0100

    Update updatinglab01_task3.py

--- updatinglab01_task3.py
+++ updatinglab01_task3.py
@@ -1,13 +1,11 @@
 import json, requests
 
 APIkey = 'b0dc5ff479faf43dff849169f51ad2b0'
-location = st.radio('Of which city do you want to know the weather?', ('Padova', 'Bressanone', 'Niscemi'))
+location = st.radio('Which city do you want to know the weather of?', ('Padova', 'Bressanone', 'Niscemi'))
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=metric'
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=metric' + '&lang=it'
 
 response = requests.get(url) 
 weatherData = json.loads(response.text)
-st.write(weatherData)
-print(weatherData['main']['temp_max'], weatherData['main']['temp_min'],'\n', weatherData['weather'][0]['description'])
 st.write('La temperatura massima a', location, 'è di', weatherData['main']['temp_max'],'°C, la temperatura minima è di', weatherData['main']['temp_min'], '°C.')
 st.write('Le condizioni sono di', weatherData['weather'][0]['description'])
