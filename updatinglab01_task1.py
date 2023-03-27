0d1b57bb39360771d1f04029784e1f78e93dc4b7
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-03-27 15:51:28 +0200

    Update updatinglab01_task1.py

--- updatinglab01_task1.py
+++ updatinglab01_task1.py
@@ -4,7 +4,7 @@ import json, requests
 APIkey = 'b0dc5ff479faf43dff849169f51ad2b0'
 location = st.text_input('Gimme a city')
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=metric
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=metric'
 
 response = requests.get(url)
 weatherData = json.loads(response.text)
