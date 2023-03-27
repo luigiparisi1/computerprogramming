5e9d89bc11512288df751fb86e9d1c56c207ab7b
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-03-27 15:48:51 +0200

    Update updatinglab01_task1.py

--- updatinglab01_task1.py
+++ updatinglab01_task1.py
@@ -8,5 +8,4 @@ url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid='
 
 response = requests.get(url)
 weatherData = json.loads(response.text)
-st.header("Weather forecast")
 st.write(weatherData['main']['temp_max'], weatherData['main']['temp_min'],'\n', weatherData['weather'][0]['description'])
