8b2fb8094d5a4ac7a7cf781e4db566abb47d380c
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-03-27 16:27:38 +0200

    Create datamuse.py

---
+++ datamuse.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import json, requests
+keyword = st.text_input('Please insert a keyword')
+criterium = st.selectbox('Please select the criterium you want to use for the search', ('Synonims', 'Antonyms', 'Sounds like', 'Means like'))
+Synonims = 'rel_syn='
+Antonyms = 'rel_ant='
+Sounds like = 'sl='
+Means like = 'ml='
+url= 'https://api.datamuse.com/words? + criterium  + keyword + '&max=10'
+response = requests.get(url)
+dataFromDatamuse = json.loads(response.text)
+st.write(dataFromDatamuse)
