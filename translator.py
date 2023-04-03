8ba2e81d427de8613b0f23f636119d8d8a72c060
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-04-03 17:22:53 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -2,5 +2,7 @@ import streamlit as st
 from googletrans import Translator
 translator = Translator()
 word = st.text_input('Gimme a word\t')
-translation = translator.translate(word, dest = st.text_input('Gimme a language using the first two letters\t'))
-st.write(translation.text)
+dest_lang = st.text_input('Gimme a language using the first two letters\t'
+if word and dest_lang:
+  translation = translator.translate(word, dest = dest_lang ))
+  st.write(translation.text)
