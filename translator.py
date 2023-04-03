b3e5050802972bf133fb36d699fbe1c0b93f0908
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-04-03 17:17:27 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -2,7 +2,5 @@ import streamlit as st
 from googletrans import Translator
 translator = Translator()
 word = st.text_input('Gimme a word\t')
-while word != 'quit':
   translation = translator.translate(word, dest = st.text_input('Gimme a language using the first two letters\t'))
   st.write(translation.text)
-  word = st.text_input('Gimme another one. If you don\'t want another word to be translated, type "quit"\t')
