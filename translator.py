70d48cd00a516fb6f18b65df7a20c19569139205
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-04-03 17:23:31 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -3,6 +3,6 @@ from googletrans import Translator
 translator = Translator()
 word = st.text_input('Gimme a word\t')
 dest_lang = st.text_input('Gimme a language using the first two letters\t'
-if word and dest_lang:
+if (word and dest_lang):
   translation = translator.translate(word, dest = dest_lang ))
   st.write(translation.text)
