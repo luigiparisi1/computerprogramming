9427eb269374c90b115f3195e517bd3388d4c566
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-04-03 17:25:32 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -4,5 +4,5 @@ translator = Translator()
 word = st.text_input('Gimme a word\t')
 dest_lang = st.text_input('Gimme a language using the first two letters\t')
 if (word and dest_lang):
-  translation = translator.translate(word, dest = dest_lang ))
+  translation = translator.translate(word, dest = dest_lang )
   st.write(translation.text)
