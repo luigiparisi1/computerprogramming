8827358662895ef2c4b4027b74e0c6a108b48893
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-04-03 18:10:04 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -2,7 +2,7 @@ import streamlit as st
 from googletrans import Translator
 translator = Translator()
 word = st.text_input('Gimme a word\t')
-dest_lang = st.text_input('Gimme a language using the first two letters\t')
+dest_lang = st.text_input('Gimme any language')
 if (word and dest_lang):
   translation = translator.translate(word, dest = dest_lang )
   st.write(translation.text)
