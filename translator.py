ccd2220fca87b6c6a8adbb363453beb36664cb1c
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-04-03 18:49:19 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -3,7 +3,7 @@ from googletrans import Translator
 st.header('This is my beautiful translator')
 translator = Translator()
 word = st.text_input('Gimme a word\t')
-dest_lang = st.selectbox('Select a language' ('German', 'Russian', 'Greek'))
+dest_lang = st.selectbox('Select a language', ('German', 'Russian', 'Greek'))
 if (word and dest_lang):
   translation = translator.translate(word, dest = dest_lang )
   st.write(translation.text)
