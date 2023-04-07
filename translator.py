58a6a3c0bdf29867ceffa136437215fea8c05b3a
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-04-07 13:11:23 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -4,5 +4,6 @@ st.header('This is my beautiful translator')
 translator = Translator()
 word = st.text_input('Gimme a word')
 dest_lang = st.text_input('Gimme any language')
+# if (word and dest_lang): dovrei metterlo ma non capisco perché
 translation = translator.translate(word, dest = dest_lang )
 st.write(translation.text)
