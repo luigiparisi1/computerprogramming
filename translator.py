8eef69f3b0f0f3b02922ea0f3c8dde263268bfb1
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-04-07 13:09:07 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -4,6 +4,6 @@ st.header('This is my beautiful translator')
 translator = Translator()
 word = st.text_input('Gimme a word')
 dest_lang = st.text_input('Gimme any language')
-if (word and dest_lang):
+
   translation = translator.translate(word, dest = dest_lang )
   st.write(translation.text)
