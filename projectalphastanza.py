059d563936872e77f606ee72615a380b8a0b3e99
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-05-16 00:51:04 +0200

    Update projectalphastanza.py

--- projectalphastanza.py
+++ projectalphastanza.py
@@ -20,7 +20,7 @@ if (input_text and dest_lang):
     lan_nlp = stanza.Pipeline(f"{dest_lang}", processors = "tokenize, mwt" )
     text = lan_nlp(translated_text)
   except stanza.pipeline.core.UnsupportedProcessorError:
-   st.info ("Sorry, this language is not supported by Stanza.", icon = 'i')
+   st.info ("Sorry, this language is not supported by Stanza.")
    text = False
 
 if text != False:
