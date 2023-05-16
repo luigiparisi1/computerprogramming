3178b956114234867e74db9ae39bc76861c286f4
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-05-16 16:43:59 +0200

    Update firsttimer.py

--- firsttimer.py
+++ firsttimer.py
@@ -3,8 +3,9 @@ import stanza
 stanza.download("it")
 it_nlp =  stanza.Pipeline('it')
 text = st.write("inserisci frase qui")
-sentence = it_nlp(text)
-for i, sent in enumerate(sentence.sentences):
+if text:
+    sentence = it_nlp(text)
+    for i, sent in enumerate(sentence.sentences):
     st.write("[Sentence {}]".format(i+1))
     for word in sent.words:
         st.info("{:12s}\t{:12s}\t{:6s}\t{:d}\t{:12s}".format(\
