a4e0a10aaeaed17b4849202cded833a81275ea95
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-05-16 19:17:38 +0200

    Update projectalphastanza.py

--- projectalphastanza.py
+++ projectalphastanza.py
@@ -13,7 +13,7 @@ name of the language in English or just type the two code-letter (e.g. for Spani
 can either type "Spanish" or "es". You will have a translation and then you can
 walk through the sentences of the translated text. When clicking on a sentence,
 you will have clickable single tokens. When clicking them, you will get the lemma and the part
-of speech of the desired word.'''
+of speech of the desired word.''')
 
 input_text = st.text_area('Please, insert text here')
 dest_lang = st.text_input('Enter a language here')
