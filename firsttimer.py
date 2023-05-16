7cd69b64dba79af5ac6a75a98a652797dde4b77d
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-05-16 16:38:36 +0200

    Update firsttimer.py

--- firsttimer.py
+++ firsttimer.py
@@ -1,3 +1,11 @@
 import streamlit as st
-title = st.text_input('Please insert a movie title', '')
-st.write('The current movie title is', title)
+import stanza
+stanza.download("it")
+it_nlp =  stanza.Pipeline('it')
+sentence = it_nlp("L'aquilone giallo è volato in cielo. Il bambino è triste per questo.")
+for i, sent in enumerate(sentence.sentences):
+    st.write("[Sentence {}]".format(i+1))
+    for word in sent.words:
+        st.info("{:12s}\t{:12s}\t{:6s}\t{:d}\t{:12s}".format(\
+              word.text, word.lemma, word.pos, word.head, word.deprel))
+    print("")
