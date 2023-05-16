bed62587ab724d1d280094cfda97a2f62fc898a7
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-05-16 16:45:37 +0200

    Update firsttimer.py

--- firsttimer.py
+++ firsttimer.py
@@ -6,7 +6,7 @@ text = st.write("inserisci frase qui")
 if text:
     sentence = it_nlp(text)
     for i, sent in enumerate(sentence.sentences):
-    st.write("[Sentence {}]".format(i+1))
+     st.write("[Sentence {}]".format(i+1))
     for word in sent.words:
         st.info("{:12s}\t{:12s}\t{:6s}\t{:d}\t{:12s}".format(\
               word.text, word.lemma, word.pos, word.head, word.deprel))
