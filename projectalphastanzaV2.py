7cda93660de57a3ccd9823dbcb1cd13950e9bdb2
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-05-17 11:53:44 +0200

    Update projectalphastanzaV2.py

--- projectalphastanzaV2.py
+++ projectalphastanzaV2.py
@@ -55,7 +55,7 @@ if text:
             st.session_state['clicked'] = i
         if st.session_state['clicked'] == i:
             st.write(f"Sentence {i+1}:")
-            for word in sent.words:
+            for word in sentence_text.words:
                 if word.pos == 'PUNCT':
                     continue
                 duplicate_avoider += 1
