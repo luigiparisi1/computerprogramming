1a35b4b94b69ac9d396eddf379723eec8fd123f9
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-05-17 18:09:58 +0200

    Update projectalphastanzaV2.py

--- projectalphastanzaV2.py
+++ projectalphastanzaV2.py
@@ -49,7 +49,7 @@ if (input_text and dest_lang):
 duplicate_avoider = 0
 if text:
  for i, sent in enumerate(text.sentences):
-   sentence_text = sent.text
+  sentence_text = sent.text
   if st.button(f"Sentence {i+1}: {sentence_text}", key=f"sentence_{i+1}"):
      st.session_state['clicked'] = i
   if st.session_state['clicked'] == i:
