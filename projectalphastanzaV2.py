ce78f9e478c78ccfb5db2e3b020a16f2fe860f8f
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-05-17 10:47:32 +0200

    Update projectalphastanzaV2.py

--- projectalphastanzaV2.py
+++ projectalphastanzaV2.py
@@ -30,7 +30,7 @@ if (input_text and dest_lang):
     output_text = translator.translate(input_text, dest=dest_lang)
     st.write(output_text.text)
     translated_text = (output_text.text)
-  except ValueError:
+ except ValueError:
     st.info (f"{dest_lang} is not a valid language!")
     text = False
     dest_lang = False
