44b4f947c0445efd210948f3fbe1ce12ace099b6
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-05-17 19:22:04 +0200

    Update projectalphastanzaV2.py

--- projectalphastanzaV2.py
+++ projectalphastanzaV2.py
@@ -53,8 +53,8 @@ if text:
  for i, sent in enumerate(text.sentences):
   sentence_text = sent.text
   if st.button(f"Sentence {i+1}: {sentence_text}", key=f"sentence_{i+1}"):
-     st.session_state['clicked'] = 1
-  if st.session_state['clicked'] == 1:
+     st.session_state['clicked'] = i+1
+  if st.session_state['clicked'] == i+1:
      st.write(f"Sentence {i+1}:")
      for x, word in enumerate(sent.words):
       if word.pos == 'PUNCT':
