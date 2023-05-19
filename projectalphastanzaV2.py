01ec2b5324f8275227382949349f290663f4a6f0
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-05-19 18:36:35 +0200

    Update projectalphastanzaV2.py

--- projectalphastanzaV2.py
+++ projectalphastanzaV2.py
@@ -27,14 +27,14 @@ st.subheader("Translation")
 if (input_text and dest_lang):
  with st.empty():
   st.info("Loading...")
-   try:
+  try:
      output_dict = translator.translate(input_text, dest=dest_lang)
      st.write(output_text.text)
      translated_text = (output_text.text)
-   except ValueError:
+  except ValueError:
      st.info (f"{dest_lang} is not a valid language!")
      text = False
-     dest_lang = None
+     dest_lang = False
 else:
   st.info ("Oops! Something is missing!")
   
