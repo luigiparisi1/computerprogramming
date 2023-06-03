f93254f7742e025930c3a1fb0bb8a2d9fedafffb
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-06-03 22:37:38 +0200

    Update projectalphastanzaV2.py

--- projectalphastanzaV2.py
+++ projectalphastanzaV2.py
@@ -76,7 +76,7 @@ if text:
   if st.session_state['clicked'] == i+1:
      st.write(f"Sentence {i+1}:")
      for x, word in enumerate(sent.words):
-      if word.pos == 'PUNCT' or 'SYM':
+      if word.pos == 'PUNCT':
        continue
       duplicate_avoider += 1
       word_text = str(word.text)
