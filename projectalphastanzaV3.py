9140b9267789c563e623f295a1f2cb06b4a5983a
PeppinoColombo <127765816+PeppinoColombo@users.noreply.github.com>
2023-06-04 10:44:00 +0200

    Update projectalphastanzaV3.py

--- projectalphastanzaV3.py
+++ projectalphastanzaV3.py
@@ -86,7 +86,7 @@ def main():
                     for x, word in enumerate(sent.words):
                         if word.pos == 'PUNCT':
                             continue
-                            duplicate_avoider += 1
+                        duplicate_avoider += 1
                         word_text = str(word.text)
                         if st.button(word_text, key=f"word_{duplicate_avoider}"):
                             lemma = word.lemma
